6f11c96519c7a3ae8d3ba636e1eb0a93e17e418d
David Raso <david.raso@cbc.ca>
2019-01-12 08:54:05 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,125 @@
+var request = require("request");
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+	Service = homebridge.hap.Service;
+	Characteristic = homebridge.hap.Characteristic;
+
+	homebridge.registerAccessory("homebridge-kunalight", "Kuna", KunaAccessory);
+}
+
+function KunaAccessory(log, config) {
+	this.log = log;
+	this.name = config["name"];
+	this.email = config["email"];
+	this.password = config["password"];
+	this.serial = config["serial"];
+	this.pollingInterval = Number(config["polling"] || 300);
+	this.authToken = "";
+	this.authURL = "https://server.kunasystems.com/api/v1/account/auth/";
+	this.statusURL = "https://server.kunasystems.com/api/v1/cameras/" + this.serial + "/";
+
+	this.service = new Service.Lightbulb(this.name);
+	
+	this.service
+		.getCharacteristic(Characteristic.On)
+		.on('get', this.getState.bind(this))
+		.on('set', this.setState.bind(this));	
+}
+
+KunaAccessory.prototype.getAuthToken = function(){
+	this.log("Getting Authentication Token...");
+		
+	request({
+	    url: this.authURL,
+	    method: "POST",
+	    json: {"email":this.email,"password":this.password}
+	},
+	
+	function (error, response, body) {
+		if (!error && response.statusCode === 200) {
+			this.log.debug(body);
+			this.authToken = body.token;
+			this.getState();
+		} else {
+			this.log("Error Getting Authentication Token - Please Check Login Credentials");
+			this.log.debug("Response Status Code: " + response.statusCode);
+			this.log.debug("Response Body" + body);
+		}
+	}.bind(this));
+}
+
+KunaAccessory.prototype.getState = function(callback) {
+	this.log("Getting current state...");
+	
+	request.get({
+		url: this.statusURL,
+		headers: { "Authorization": "Token " + this.authToken }
+	},
+
+	function (error, response, body) {
+		if (!error && response.statusCode === 200) {
+			this.log.debug(body);
+			var json = JSON.parse(body);
+			var state = json.bulb_on;
+			
+			this.log("Light is currently %s", state? "ON": "OFF");
+			this.service.getCharacteristic(Characteristic.On).updateValue(state);
+			if (typeof callback !== 'undefined') callback(null, state);
+
+		} else {
+			this.log.debug("Response Status Code: " + response.statusCode);
+			this.log.debug("Response Body" + body);
+			if (response.statusCode === 401) {
+				console.log("Authorization Token Expired Getting New One");
+				this.getAuthToken();
+			}
+			if (typeof callback !== 'undefined') callback(err);
+		}
+	}.bind(this));
+}
+  
+KunaAccessory.prototype.setState = function(state, callback) {
+	this.log("Setting State...");
+	
+	request({
+		url: this.statusURL,
+		method: "PATCH",
+		json: {"bulb_on":state},
+		headers: { "Authorization": "Token " + this.authToken }
+	},
+
+	function (error, response, body) {
+		if (!error && response.statusCode === 200) {
+			this.log.debug(body);
+			var state = body.bulb_on;
+			
+			this.log("Light is currently %s", state? "ON": "OFF");
+			this.service.getCharacteristic(Characteristic.On).updateValue(state)
+			if (typeof callback !== 'undefined') callback(null, state);
+
+		} else {
+			this.log.debug("Response Status Code: " + response.statusCode);
+			this.log.debug("Response Body" + body);
+			if (response.statusCode === 401) {
+				console.log("Authorization Token Expired Getting New One");
+				this.getAuthToken();
+			}
+			if (typeof callback !== 'undefined') callback(err);
+		}
+	}.bind(this))	
+}
+
+KunaAccessory.prototype.getServices = function() {
+	this.log.debug("getServices");
+	
+	if (this.pollingInterval > 0) {
+		this.timer = setInterval(
+			this.getState.bind(this),
+			this.pollingInterval * 1000
+		);
+	}
+
+	this.getAuthToken();
+	return [this.service];
+}
